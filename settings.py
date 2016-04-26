a83e2fac91ececb6b28b5a203d0734d37fca4695
Matt Butterfield <matt.butterfield@percolate.com>
2016-04-26 14:11:25 -0400

    dont use cwd

--- settings.py
+++ settings.py
@@ -5,8 +5,10 @@ DB_URI = 'postgresql://localhost/reddit_scraper'
 
 REDDIT_USER_AGENT = 'Reddit Scraper Script'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 IMAGES_FOLDER_NAME = 'scraped_images'
-IMAGES_FOLDER_PATH = os.path.join(os.getcwd(), IMAGES_FOLDER_NAME)
+IMAGES_FOLDER_PATH = os.path.join(PROJECT_ROOT, IMAGES_FOLDER_NAME)
 
 IMGUR_CLIENT_ID = os.getenv('IMGUR_CLIENT_ID')
 IMGUR_CLIENT_SECRET = os.getenv('IMGUR_CLIENT_SECRET')
