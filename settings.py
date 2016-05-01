5c28e53f643dda262862c937d676910e17374721
Matt Butterfield <matt.butterfield@percolate.com>
2016-05-01 17:23:34 -0400

    remove project root

--- settings.py
+++ settings.py
@@ -9,8 +9,6 @@ DB_URI = os.getenv('REDDIT_SCRAPER_DB_URI')
 
 REDDIT_USER_AGENT = 'Reddit Scraper Script'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 IMAGES_FOLDER_PATH = os.getenv('REDDIT_SCRAPER_IMAGES_PATH')
 
 IMGUR_CLIENT_ID = os.getenv('IMGUR_CLIENT_ID')
