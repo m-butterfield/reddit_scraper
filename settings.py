d06332bc96d40dd052a0a31a1fa33636e3523e1d
Matt Butterfield <matt.butterfield@percolate.com>
2016-05-01 16:45:49 -0400

    manage test images dir

--- settings.py
+++ settings.py
@@ -5,14 +5,13 @@ Project settings
 import os
 
 
-DB_URI = os.getenv("REDDIT_SCRAPER_DB_URI")
+DB_URI = os.getenv('REDDIT_SCRAPER_DB_URI')
 
 REDDIT_USER_AGENT = 'Reddit Scraper Script'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-IMAGES_FOLDER_NAME = 'scraped_images'
-IMAGES_FOLDER_PATH = os.path.join(PROJECT_ROOT, IMAGES_FOLDER_NAME)
+IMAGES_FOLDER_PATH = os.getenv('REDDIT_SCRAPER_IMAGES_PATH')
 
 IMGUR_CLIENT_ID = os.getenv('IMGUR_CLIENT_ID')
 IMGUR_CLIENT_SECRET = os.getenv('IMGUR_CLIENT_SECRET')
