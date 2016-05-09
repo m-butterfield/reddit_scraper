c21e790d1a839e9446611cf79c2f54577e894de3
Matt Butterfield <matt.butterfield@percolate.com>
2016-05-09 09:50:23 -0400

    docopt

---
+++ reddit_scraper.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/docopt/docopt-go"
+)
+
+func main() {
+	usage := `
+reddit_scraper
+
+Scrape images from a subreddit.
+
+Usage:
+  reddit_scraper initialize
+  reddit_scraper backfill <subreddit> <date>
+  reddit_scraper scrape <subreddit>
+
+Options:
+  -h --help              Show this screen
+`
+	arguments, _ := docopt.Parse(usage, nil, true, "", false)
+	fmt.Println(arguments)
+}
